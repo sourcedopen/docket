7efa00fdd0ef64f925892e309c2d79f3291f31e2
Chinmay Purav <chinpurav@gmail.com>
2026-02-21 19:17:27 +0530

    feat: configure app service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,11 @@ namespace App\Providers;
 
 use App\Models\Ticket;
 use App\Observers\TicketObserver;
+use Carbon\CarbonImmutable;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Validation\Rules\Password;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -12,6 +16,31 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        $this->configureCommands();
+        $this->configurePasswordValidation();
+        $this->configureDates();
+        $this->configureObservers();
+    }
+
+    private function configureObservers(): void
+    {
         Ticket::observe(TicketObserver::class);
+    }
+
+    private function configureDates(): void
+    {
+        Date::use(CarbonImmutable::class);
+    }
+
+    private function configureCommands(): void
+    {
+        DB::prohibitDestructiveCommands(
+            $this->app->isProduction()
+        );
+    }
+
+    private function configurePasswordValidation(): void
+    {
+        Password::defaults(fn () => $this->app->isProduction() ? Password::min(8)->uncompromised() : null);
     }
 }
